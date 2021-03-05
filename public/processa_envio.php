ad19653115b244c24a406cf93e69b02094c9db0a
Guilherme Feitosa <guifcm12@gmail.com>
2021-03-05 10:28:37 -0300

    First commit

---
+++ public/processa_envio.php
@@ -0,0 +1,9 @@
+<?php
+
+	require "../src/bibliotecas/PHPMailer/Exception.php";
+	require "../src/bibliotecas/PHPMailer/OAuth.php";
+	require "../src/bibliotecas/PHPMailer/PHPMailer.php";
+	require "../src/bibliotecas/PHPMailer/POP3.php";
+	require "../src/bibliotecas/PHPMailer/SMTP.php";
+
+	require "../src/processa_envio.php";
